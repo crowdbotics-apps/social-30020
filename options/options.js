dae7cae3a9cc22e4fe44b33884fa5bcb88326ba1
Crowdbotics <team@crowdbotics.com>
2021-08-22 11:30:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "social_30020Identifier",
+  url: "https://social_30020Identifier.botics.co",
+  api: "https://social_30020Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
